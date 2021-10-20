0c59a69447a8db4d5f4b4a88a6d44317449c2362
Lukaz06 <92752089+Lukaz06@users.noreply.github.com>
2021-10-20 20:33:56 -0300

    Add files via upload

---
+++ Projeto.c
@@ -0,0 +1,89 @@
+///////////////////////////////////////////////////////////////////////////////
+///             Universidade Federal do Rio Grande do Norte                 ///
+///             Centro de Ensino Superior do Seridó                         ///
+///             Departamento de Computação e Tecnologia                     ///
+///             Disciplina DCT1106 -- Programação                           ///
+///             Projeto Uma Agenda de Tarefas e Compromissos                ///
+///             Developed by  @lukaz06 - Out, 2021                          ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+// Assinatura das funções
+    void telaSobre();
+    void telaPrincipal();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CódigoPrincipal 
+
+#include <stdio.h> 
+int main() { 
+    telaSobre();
+    telaPrincipal();
+//(apresentação de telas)
+    return 0;
+} 
+
+
+// Funções 
+void telaSobre() {
+printf("\n"); 
+printf("/////////////////////////////////////////////////////////////////////////////////\n");
+printf("///                                                                           ///\n");
+printf("///     = = = = = Projeto Uma Agenda de Tarefas e Compromissos = = = = =      ///\n");
+printf("///                                                                           ///\n");
+printf("///  Um programa de computador que realiza o agendamento de tarefas, de forma ///\n");
+printf("///  eficiente, fornecendo lembretes ao usuario sobre compromissos agendados. ///\n");
+printf("/////////////////////////////////////////////////////////////////////////////////\n");
+printf("\n");
+} 
+
+
+
+void telaPrincipal() {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///                Universidade Federal do Rio Grande do Norte              ///\n");
+    printf("///                Centro de Ensino Superior do Serido                      ///\n");
+    printf("///                Departamento de Computaçao e Tecnologia                  ///\n");
+    printf("///                Disciplina DCT1106 -- Programaçao                        ///\n");
+    printf("///                Projeto Uma Agenda de Tarefas e Compromissos             ///\n");
+    printf("///                Developed by  @lukaz06 - Out, 2021                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///        = = = = = Agenda de Tarefas e Compromissos = = = = =             ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            1. Adicionar Lembrentes                                      ///\n");
+    printf("///            2. Ver Lembrentes                                            ///\n");
+    printf("///            3. Editar Lembrentes                                         ///\n");
+    printf("///            4. Apagar Lembrentes                                         ///\n");
+    printf("///            0. Sair                                                      ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
